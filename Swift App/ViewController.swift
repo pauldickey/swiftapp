38c296df99794fd52bf3fb38ff6bcf6c2c2baab7
Paul Dickey <pauldickey@Pauls-MacBook-Pro.local>
2017-09-04 13:53:41 -0700

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Paul Dickey on 9/4/17.
+//  Copyright © 2017 Paul Dickey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
